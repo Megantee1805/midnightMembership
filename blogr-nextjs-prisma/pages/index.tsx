30e579f8fc9b03102f31b0a9d60f1eea49074545
Megantee1805 <megan.tee1805@gmail.com>
2022-03-13 17:01:09 +0800

    ok

--- blogr-nextjs-prisma/pages/index.tsx
+++ blogr-nextjs-prisma/pages/index.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma from '../lib/prisma'
+import Registration from "../components/registration"
 
 // index.tsx
 export const getStaticProps: GetStaticProps = async () => {
@@ -22,32 +23,33 @@ type Props = {
 
 const Blog: React.FC<Props> = (props) => {
   return (
-    <Layout>
-      <div className="page">
-        <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
+    <Registration></Registration>
+    // <Layout>
+    //   <div className="page">
+    //     <h1>Public Feed</h1>
+    //     <main>
+    //       {props.feed.map((post) => (
+    //         <div key={post.id} className="post">
+    //           <Post post={post} />
+    //         </div>
+    //       ))}
+    //     </main>
+    //   </div>
+    //   <style jsx>{`
+    //     .post {
+    //       background: white;
+    //       transition: box-shadow 0.1s ease-in;
+    //     }
 
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
+    //     .post:hover {
+    //       box-shadow: 1px 1px 3px #aaa;
+    //     }
 
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
-    </Layout>
+    //     .post + .post {
+    //       margin-top: 2rem;
+    //     }
+    //   `}</style>
+    // </Layout>
   )
 }
 
